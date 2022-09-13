d3af09360fe053e23b2625a7056f33afc66eb664
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2022-09-13 14:51:43 +0800

    Add test for Monoid chapter

--- src/ChMonoid/Optional.hs
+++ src/ChMonoid/Optional.hs
@@ -1,5 +1,5 @@
 module ChMonoid.Optional (module ChMonoid.Optional) where
-
+import Test.QuickCheck
 data Optional a = Nada | Only a deriving (Eq, Show)
 
 instance Semigroup  a => Semigroup (Optional a) where
@@ -19,3 +19,86 @@ instance Semigroup Bull where
 
 instance Monoid Bull where
     mempty = Fools
+
+newtype First' a = First' { getFirst' :: Optional a }
+    deriving (Eq, Show)
+
+instance Semigroup (First' a) where
+    (First' x) <> (First' Nada) = First' x
+    (First' Nada) <> (First' x) = First' x
+    (First' x) <> (First' _)=  First' x
+
+instance Monoid (First' a) where
+    mempty = First' Nada
+
+firstMappend :: First' a -> First' a -> First' a
+firstMappend = mappend
+
+instance Arbitrary Bull where
+  arbitrary = frequency [ (1, return Fools) , (1, return Twoo) ]
+
+instance Arbitrary a => Arbitrary (First' a) where
+  arbitrary = do
+    x <- arbitrary
+    frequency [ (1, return (First' Nada)), (1, return (First' (Only x)))]
+
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+    Trivial <> Trivial = Trivial
+
+instance Arbitrary Trivial where
+    arbitrary = return Trivial
+
+newtype Identity a = Identity a
+    deriving (Eq, Show)
+
+instance Semigroup (Identity a) where
+    (Identity x) <> (Identity _) = Identity x
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    Identity <$> arbitrary
+
+newtype BoolConj = BoolConj Bool
+    deriving (Eq, Show)
+
+instance Semigroup BoolConj where
+    (BoolConj True) <> (BoolConj True) = BoolConj True
+    _ <> _ = BoolConj False
+
+instance Arbitrary BoolConj where
+  arbitrary = frequency [ (1, return $ BoolConj False) , (1, return $ BoolConj True) ]
+
+newtype BoolDisj = BoolDisj Bool
+    deriving (Eq, Show)
+
+instance Semigroup BoolDisj where
+    (BoolDisj False) <> (BoolDisj False) = BoolDisj False
+    _ <> _ = BoolDisj True
+
+instance Arbitrary BoolDisj where
+  arbitrary = frequency [ (1, return $ BoolDisj False) , (1, return $ BoolDisj True) ]
+
+data Or a b = Fst a | Snd b
+    deriving (Eq, Show)
+
+instance Semigroup (Or a b) where
+    (Fst _) <> (Fst y) = Fst y
+    (Fst _) <> (Snd y) = Snd y
+    (Snd x) <> _ = Snd x
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    frequency [(1, return (Fst x)), (1, return (Snd y))]
+
+newtype Combine a b = Combine { unCombine :: a -> b }
+
+instance Semigroup b => Semigroup (Combine a b) where
+    (Combine f) <> (Combine g) = Combine (\x -> f x <> g x)
+
+instance Arbitrary b => Arbitrary (Combine a b) where
+  arbitrary = do
+    Combine . const <$> arbitrary
--- test/ChMonoid/OptionalSpec.hs
+++ test/ChMonoid/OptionalSpec.hs
@@ -8,11 +8,18 @@ module ChMonoid.OptionalSpec (spec) where
 import Test.Hspec
 import Data.Monoid
 import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck
 import ChMonoid.Optional
+import Test.QuickCheck (quickCheck)
 
-monoidAssoc :: (Monoid m, Show m, Eq m) => m -> m -> m -> Expectation
-monoidAssoc a b c = (a <> b) <> c `shouldBe` a <> (b <> c)
+semigroupAssoc :: (Semigroup m, Show m, Eq m) => m -> m -> m -> Expectation
+semigroupAssoc a b c = (a <> b) <> c `shouldBe` a <> (b <> c)
+
+semigroupAssocFun :: (Semigroup b, Show b, Eq b) => b -> b -> b -> a -> Expectation
+semigroupAssocFun x y z d = (unCombine $ (f <> g) <> h) d `shouldBe` (unCombine $ f <> (g <> h)) d
+  where
+    f = Combine (const x)
+    g = Combine (const y)
+    h = Combine (const z)
 
 monoidLeftIdentity :: (Eq m, Show m, Monoid m) => m -> Expectation
 monoidLeftIdentity a = (mempty <> a) `shouldBe` a
@@ -20,15 +27,13 @@ monoidLeftIdentity a = (mempty <> a) `shouldBe` a
 monoidRightIdentity :: (Eq m, Show m, Monoid m) => m -> Expectation
 monoidRightIdentity a = (a <> mempty) `shouldBe` a
 
-instance Arbitrary Bull where
-    arbitrary = frequency [ (1, return Fools) , (1, return Twoo) ]
-
 spec :: Spec
 spec = do
-  let onlySum = Only (Sum 1)
+  describe "Testing Only with Monoids" $ do
+    let
+      onlySum = Only (Sum 1)
       onlyFour = Only (Product 4)
       onlyTwo = Only (Product 2)
-  describe "Testing Only with Monoids" $ do
     it "Sum" $ do
       onlySum `mappend` onlySum `shouldBe` Only (Sum {getSum = 2})
     it "Product" $ do
@@ -40,12 +45,78 @@ spec = do
     it "Nada <> Only a" $ do
       Nada `mappend` Only (Sum 1) `shouldBe` Only (Sum {getSum = 1})
 
+  describe "Testing Semigroup First'" $ do
+    let
+      onlyOne = First' (Only 1)
+      onlyTwo = First' (Only 2)
+      nada = First' Nada :: First' Integer
+    it "onlyOne <> Nada" $ do
+      onlyOne `mappend` nada `shouldBe` First' {getFirst' = Only 1}
+    it "Nada <> Nada" $ do
+      nada `mappend` nada `shouldBe` First' {getFirst' = Nada}
+    it "Nada <> onlyTwo" $ do
+      nada `mappend` onlyTwo `shouldBe` First' {getFirst' = Only 2}
+    it "onlyOne <> onlyTwo" $ do
+      onlyOne `mappend` onlyTwo `shouldBe` First' {getFirst' = Only 1}
+
+  describe "Testing Semigroup Or" $ do
+    it "Fst 1 <> Snd 2" $ do
+      Fst 1 <> Snd 2 `shouldBe` Snd 2
+    it "Fst 1 <> Fst 2" $ do
+      Fst 1 <> Fst 2 `shouldBe` (Fst 2 :: Or Integer Integer)
+    it "Snd 1 <> Fst 2" $ do
+      Snd 1 <> Fst 2 `shouldBe` Snd 1
+    it "Snd 1 <> Snd 2" $ do
+      Snd 1 <> Snd 2 `shouldBe` (Snd 1 :: Or Integer Integer)
+
+  describe "Testing Semigroup Combine" $ do
+    let
+      f = Combine $ \n -> Sum (n + 1)
+      g = Combine $ \n -> Sum (n - 1)
+    it "(f <> g) 0" $ do
+      unCombine (f <> g) 0 `shouldBe` Sum {getSum = 0}
+    it "(f <> g) 1" $ do
+      unCombine (f <> g) 1 `shouldBe` Sum {getSum = 2}
+    it "(f <> f) 1" $ do
+      unCombine (f <> f) 1 `shouldBe` Sum {getSum = 4}
+    it "(g <> f) 1" $ do
+      unCombine (g <> f) 1 `shouldBe` Sum {getSum = 2}
+
   context "Monoid String" $ do
-    prop "associative" (monoidAssoc :: String -> String -> String -> Expectation)
+    prop "associative" (semigroupAssoc :: String -> String -> String -> Expectation)
     prop "Left Identity" (monoidLeftIdentity :: String -> Expectation)
     prop "Right Identity" (monoidRightIdentity :: String -> Expectation)
 
   context "Monoid Bull" $ do
-    prop "associative" (monoidAssoc :: Bull -> Bull -> Bull -> Expectation)
+    prop "associative" (semigroupAssoc :: Bull -> Bull -> Bull -> Expectation)
     prop "Left Identity" (monoidLeftIdentity :: Bull -> Expectation)
     prop "Right Identity" (monoidRightIdentity :: Bull -> Expectation)
+
+  context "Monoid First'" $ do
+    prop "associative" (semigroupAssoc :: First' Integer -> First' Integer -> First' Integer-> Expectation)
+    prop "Left Identity" (monoidLeftIdentity :: First' Integer -> Expectation)
+    prop "Right Identity" (monoidRightIdentity :: First' Integer -> Expectation)
+
+  describe "Type Trivial" $ do
+    context "Semigroup properties" $ do
+      prop "associative" (semigroupAssoc :: Trivial -> Trivial -> Trivial -> Expectation)
+
+  describe "Type Identity" $ do
+    context "Semigroup properties" $ do
+      prop "associative" (semigroupAssoc :: Identity Integer -> Identity Integer -> Identity Integer -> Expectation)
+
+  describe "Type BoolConj" $ do
+    context "Semigroup properties" $ do
+      prop "associative" (semigroupAssoc :: BoolConj -> BoolConj -> BoolConj -> Expectation)
+
+  describe "Type BoolDisj" $ do
+    context "Semigroup properties" $ do
+      prop "associative" (semigroupAssoc :: BoolDisj -> BoolDisj -> BoolDisj -> Expectation)
+
+  describe "Type Or" $ do
+    context "Semigroup properties" $ do
+      prop "associative" (semigroupAssoc :: Or Integer Integer -> Or Integer Integer -> Or Integer Integer -> Expectation)
+
+  describe "Type Combine" $ do
+    it "Semigroup properties" $ do
+      quickCheck (semigroupAssocFun :: String -> String -> String -> Integer -> Expectation)
